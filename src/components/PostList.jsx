c3b272357dda7da5b5c855363957e1a22b360637
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-10-21 14:17:58 +0200

    improved the elements for Item component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,19 @@
+// module for formating dates
+import moment from 'moment';
+
 function Item({ post }) {
   return (
     <article className="item">
-      <h2>{post.title}</h2>
+      <h3>{post.title}</h3>
       <p>{post.author.name}</p>
+      <img
+        // removes the "?size=50x50&set=set1 from the string"
+        // otherwise the link did not work
+        src={post.author.avatar.slice(0, post.author.avatar.indexOf('?'))}
+        alt="avatar"
+      />
+      {/* moment formats the date in a "28th of September 2020" format*/}
+      <p>{moment(post.publishDate).format('Do of MMMM YYYY')}</p>
     </article>
   );
 }
