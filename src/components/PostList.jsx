5e30b73d0cef245ccc8a7c1f71a7df52086aee3e
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-10-21 11:01:42 +0200

    created PostList component, which shows posts data in the browser

---
+++ src/components/PostList.jsx
@@ -0,0 +1,12 @@
+export default function PostList({ posts }) {
+  return (
+    <div className="post-list">
+      {posts.map((post) => (
+        <div className="post-preview" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.author.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
