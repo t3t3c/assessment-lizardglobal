34343de8c98eeb2a8f3a00a85f73a3a9b1ec741a
t3t3c <65117237+t3t3c@users.noreply.github.com>
2022-10-21 20:54:54 +0200

    diffrent side logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { BsGithub } from 'react-icons/bs';
 export default function Footer() {
   return (
     <footer>
-      <p>Website made by Hubert Gajewski</p>
       <a href="https://github.com/t3t3c">
         <BsGithub size="1.6em" />
       </a>
+      <p>Website made by Hubert Gajewski</p>
     </footer>
   );
 }
